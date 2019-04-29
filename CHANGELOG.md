611d6186a910f7cecd1e3189c9833398bcb28690
Rafael Fernández <rafael89.fernandez@gmail.com>
2019-04-29 11:37:56 +0200

    First commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## [1.0.0 Primera versión](https://svrgitpub.sdos.es/iOS/SDOSCustomLoader/tree/v1.0.0)
+
+- Primera versión de la librería
---
+++ Sample/SDOSCustomLoader/App/Others/AutoGenerate/StoryboardConstants.m
@@ -0,0 +1,26 @@
+//  FICHERO AUTOGENERADO - NO MODIFICAR
+//  StoryboardConstants.m
+//
+//  Created by SDOS
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@implementation UINavigationController (ExampleLoader)
+
+@end
+@implementation DocumentationLoaderViewController (ExampleLoader)
+
+@end
+
+// Auto generated file - any changes will be lost
+
+
+#pragma mark - navigationController.storyboardIdentifier
+NSString * const ExampleLoaderNavigationController = @"ExampleLoaderNavigationController";
+
+#pragma mark - storyboardNames
+NSString * const ExampleLoader = @"ExampleLoader";
+
+
